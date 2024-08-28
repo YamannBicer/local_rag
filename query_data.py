ebf3e515cb36a7de4c7b0a7731f27c42483bcc98
Yaman Biçer <162802152+YamannBicer@users.noreply.github.com>
2024-08-28 18:26:55 +0300

    ewms nedire doğru cevap verdi. hangi chunklara baktığnı göstermiyor. chroma da sıkıntı yok llama3.1 ve nomic-embed-textkullanıyor

--- query_data.py
+++ query_data.py
@@ -1,5 +1,5 @@
 import argparse
-from langchain.vectorstores.chroma import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
 
@@ -30,7 +30,7 @@ def query_rag(query_text: str):
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
     # Search the DB.
-    results = db.similarity_search_with_score(query_text, k=5)
+    results = db.similarity_search_with_score(query_text, k=3)
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
